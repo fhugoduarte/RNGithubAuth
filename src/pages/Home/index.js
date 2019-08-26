5173dcceac3fff276f2a6b799734f593508f3876
fhugoduarte <h.duarte222@gmail.com>
2019-08-25 21:03:02 -0300

    chore: Finish project

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,62 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useEffect, useState, Fragment} from 'react';
+import {View, Image, Text, StyleSheet, ActivityIndicator} from 'react-native';
+import axios from 'axios';
 
-export default function Home() {
-  return <Text>Home</Text>;
+export default function Home({navigation}) {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    async function getUser() {
+      const token = navigation.getParam('token');
+      const response = await axios.get('https://api.github.com/user', {
+        headers: {
+          Authorization: `token ${token}`,
+        },
+      });
+      setUser(response.data);
+    }
+
+    getUser();
+  }, [navigation]);
+
+  return (
+    <View style={styles.container}>
+      {user ? (
+        <Fragment>
+          <Image style={styles.avatar} source={{uri: user.avatar_url}} />
+          <Text style={styles.userName}>{user.name}</Text>
+          <Text style={styles.userBio}>{user.bio}</Text>
+        </Fragment>
+      ) : (
+        <ActivityIndicator color="#333" />
+      )}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  avatar: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+    borderWidth: 2,
+    borderColor: '#333',
+    margin: 20,
+  },
+  userName: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  userBio: {
+    fontSize: 16,
+    color: '#666',
+    textAlign: 'center',
+  },
+});
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,6 +1,98 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  Text,
+  Platform,
+  Linking,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  ActivityIndicator,
+} from 'react-native';
+import axios from 'axios';
+import {words} from 'lodash';
+import {CLIENT_ID, CLIENT_SECRET} from 'react-native-dotenv';
 
-export default function SignIn() {
-  return <Text>SigIn</Text>;
+export default function SignIn({navigation}) {
+  const [loading, setLoading] = useState(false);
+  const [callbackCode, setCallbackCode] = useState(null);
+
+  useEffect(() => {
+    function handleOpenURL(url) {
+      if (url) {
+        setLoading(true);
+        const [, code] = url.split('code=');
+        setCallbackCode(code);
+      }
+    }
+
+    if (Platform.OS === 'android') {
+      Linking.getInitialURL().then(url => handleOpenURL(url));
+    } else {
+      Linking.addEventListener('url', ({url}) => handleOpenURL(url));
+    }
+
+    return () => Linking.removeEventListener('url');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    async function getToken(code) {
+      try {
+        const {data} = await axios.post(
+          'https://github.com/login/oauth/access_token',
+          {
+            client_id: CLIENT_ID,
+            client_secret: CLIENT_SECRET,
+            code,
+          },
+        );
+        if (!data.includes('error')) {
+          const [, token] = words(data, /\=(.*?)\&/);
+          navigation.navigate('Home', {token});
+        }
+      } catch (error) {
+        console.log('error', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    if (callbackCode) {
+      getToken(callbackCode);
+    }
+  }, [callbackCode, navigation]);
+
+  return (
+    <View>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() =>
+          Linking.openURL(
+            `https://github.com/login/oauth/authorize?scope=user,email&client_id=${CLIENT_ID}`,
+          )
+        }>
+        {loading ? (
+          <ActivityIndicator color="#fff" />
+        ) : (
+          <Text style={styles.title}>Sign In With Github</Text>
+        )}
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#333',
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    marginHorizontal: 30,
+    borderRadius: 5,
+  },
+  title: {
+    fontWeight: '400',
+    color: '#fff',
+  },
+});
